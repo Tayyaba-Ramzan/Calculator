24c8e0d6518736f7e17e6497283dbd4ef85513ca
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-04-20 20:11:04 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,6 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
-console.log(chalk.bgGreen.bold.italic.red("WELCOME MY CALCULATOR PROJECT"));
-console.log(chalk.white.bold("___________________________________________________________"));
-console.log();
-
 let answer = await inquirer.prompt([
     { message: "Enter First Number", type: "number", name: "firstNumber" },
     { message: "Select one of the operators to perform operation", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Multiplication", "Division", "Reminder", "Square Root", "Factorial"] }
@@ -13,36 +9,33 @@ let answer = await inquirer.prompt([
 
 if (answer.operator === "Addition") {  // Addition Operands
     let secondNumber = await inquirer.prompt({ message: "Enter Second Number", type: "number", name: "secondNumber" });
-    console.log(chalk.bgYellow.black.bold.italic("Result: ", answer.firstNumber + secondNumber.secondNumber));
+    console.log(chalk.magenta("Result: ", answer.firstNumber + secondNumber.secondNumber));
 } else if (answer.operator === "Subtraction") {  // Subtraction Operands
     let secondNumber = await inquirer.prompt({ message: "Enter Second Number", type: "number", name: "secondNumber" });
-    console.log(chalk.bgYellow.black.bold.italic("Result: ", answer.firstNumber - secondNumber.secondNumber));
+    console.log(chalk.blue("Result: ", answer.firstNumber - secondNumber.secondNumber));
 
 } else if ((answer.operator === "Multiplication")) {  // Multipication Operands
     let secondNumber = await inquirer.prompt({ message: "Enter Second Number", type: "number", name: "secondNumber" });
-    console.log(chalk.bgYellow.black.bold.italic("Result: ", answer.firstNumber * secondNumber.secondNumber));
+    console.log(chalk.green("Result: ", answer.firstNumber * secondNumber.secondNumber));
 
 } else if (answer.operator === "Division") {  // Divsion Operands
     let secondNumber = await inquirer.prompt({ message: "Enter Second Number", type: "number", name: "secondNumber" });
-    console.log(chalk.bgYellow.black.bold.italic("Result: ", answer.firstNumber / secondNumber.secondNumber));
+    console.log(chalk.red("Result: ", answer.firstNumber / secondNumber.secondNumber));
 
 } else if (answer.operator === "Reminder") {  //Reminder Operands
     let secondNumber = await inquirer.prompt({ message: "Enter Second Number", type: "number", name: "secondNumber" });
-    console.log(chalk.bgYellow.black.bold.italic("Result: ", answer.firstNumber % secondNumber.secondNumber));
+    console.log(chalk.yellow("Result: ", answer.firstNumber % secondNumber.secondNumber));
 
 } else if (answer.operator === "Square Root") {  // Squrae Root Operands
-    console.log(chalk.bgYellow.black.bold.italic("Result: ", Math.sqrt(answer.firstNumber)));
+    console.log(chalk.white("Result: ", Math.sqrt(answer.firstNumber)));
 
 } else if (answer.operator === "Factorial") {  // Factorial Operands
     let factorial = 1;
     for (let i = 2; i <= answer.firstNumber; i++) {
         factorial *= i;
     }
-    console.log(chalk.bgYellow.black.bold.italic("Result: ", factorial));
+    console.log(chalk.cyan("Result: ", factorial));
 } else {
-    console.log(chalk.bgYellow.black("Pleased! Select valid Operator."));
+    console.log(chalk.gray("Pleased! Select valid Operator."));
 
 }
-
-
-
